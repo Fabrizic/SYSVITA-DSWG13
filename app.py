fef71fe08d58e538b584bbf63fec78fe189a7f38
Fabrizic <iamfabrizic@gmail.com>
2024-06-12 11:27:08 -0500

    Add CORs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from utils.db import db
 from services.respuestas_usuarios_routes import respuestas_usuarios_routes
 from services.respuestas_login_routes import respuestas_login_routes
@@ -6,6 +7,7 @@ from services.respuestas_register_routes import respuestas_register_routes
 from config import DATABASE_CONNECTION
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:4200'])
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
