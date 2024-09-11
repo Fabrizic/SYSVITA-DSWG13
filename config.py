169c6e53ad45bb52e8fc3bbfbcbaa259bed4274e
Fabrizic <iamfabrizic@gmail.com>
2024-09-11 18:01:44 -0500

    Prueba

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-DATABASE_CONNECTION= "postgresql://fabrizic:TNj77P50Dvd4KHdDp4ZRCr9nFKFlKLsh@dpg-cp4hkgi1hbls73f20ef0-a.oregon-postgres.render.com/dswg13"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user=os.environ['USER']
+pwd=os.environ['PASSWORD']
+host=os.environ['HOST']
+database=os.environ['DATABASE']
+server=os.environ['SERVER']
+
+DATABASE_CONNECTION=f'{server}://{user}:{pwd}@{host}/{database}'
