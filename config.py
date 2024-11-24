a72a3103ec54c7bae6b63ba8f27f8c1c9ac45397
Fabrizic <iamfabrizic@gmail.com>
2024-11-24 11:55:53 -0500

    Actualizar la cadena de conexión a la base de datos en config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ pwd=os.environ['PASSWORD']
 host=os.environ['HOST']
 database=os.environ['DATABASE']
 server=os.environ['SERVER']
-DATABASE_CONNECTION=f'{server}://{user}:{pwd}@{host}/{database}'
+DATABASE_CONNECTION = 'postgresql://sisvita_user:xwjtQgxHA0uws48NJbq1qn2qDHQp6fIq@dpg-ct1aqou8ii6s73fe6q20-a.oregon-postgres.render.com/sisvita_6sgi'
