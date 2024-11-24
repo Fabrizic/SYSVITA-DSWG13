e285aae887020e0b28134fe40c9d820f5d5b25e3
Fabrizic <iamfabrizic@gmail.com>
2024-11-24 11:27:05 -0500

    Eliminar el puerto de la cadena de conexión a la base de datos en config.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
 user=os.environ['USER']
 pwd=os.environ['PASSWORD']
 host=os.environ['HOST']
-port = os.environ['PORT']
 database=os.environ['DATABASE']
 server=os.environ['SERVER']
-
-DATABASE_CONNECTION=f'{server}://{user}:{pwd}@{host}:{port}/{database}'
+DATABASE_CONNECTION=f'{server}://{user}:{pwd}@{host}/{database}'
